22e85b49ec20b94e31fa6574cbc32b0a7d6e3d7b
Allan Neri <allan.nerifs@gmail.com>
2019-02-28 16:54:00 -0300

    Promocao
    Adicionado Controlle, Service e Repository da classe Promocao

---
+++ src/main/java/com/github/allanfs/dgapp/modelo/Promocao.java
@@ -0,0 +1,27 @@
+package com.github.allanfs.dgapp.modelo;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="tb_promocao")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor @AllArgsConstructor
+public class Promocao {
+
+	@Getter @Setter private String nome;
+	@Getter @Setter private Date dataInicio;
+	@Getter @Setter private Date dataFim;
+	
+	@Getter @Setter private Set<Produto> produtos;
+}
---
+++ src/main/java/com/github/allanfs/dgapp/repository/PromocaoRepository.java
@@ -0,0 +1,10 @@
+package com.github.allanfs.dgapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.github.allanfs.dgapp.modelo.Promocao;
+
+public interface PromocaoRepository extends JpaRepository<Promocao, Long>{
+
+    
+}
