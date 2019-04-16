00e4d5f17e6fc32f4493296da436cd398f27bc25
Allan Neri Fernandes da Silva <allan.nerifs@gmail.com>
2019-04-16 20:14:11 -0300

    Conexões docker

--- src/main/java/com/github/allanfs/dgapp/service/PedidoService.java
+++ src/main/java/com/github/allanfs/dgapp/service/PedidoService.java
@@ -3,10 +3,12 @@ package com.github.allanfs.dgapp.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.github.allanfs.dgapp.modelo.Pedido;
 import com.github.allanfs.dgapp.repository.PedidoRepository;
 
+@Service
 public class PedidoService implements IService<Pedido> {
 
 	@Autowired
