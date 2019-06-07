03dac12074cb640afd71468d8e8b581fe66da5e8
Allan Neri Fernandes da Silva <allan.fernandes@dataprev.gov.br>
2019-06-07 12:09:38 -0300

    Adicionado nova classe para rastrear posição de um recheio em um sabor #4

---
+++ src/main/java/com/github/allanfs/dgapp/dgapp/pizza/model/SaborOrdemRecheioEmbeddedId.java
@@ -0,0 +1,67 @@
+package com.github.allanfs.dgapp.dgapp.pizza.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Dentro de uma classe sabor, os recheios 
+ * possuem posições especificas.
+ * 
+ * Ex.: Molho e mussarela são os primeiros
+ * recheios em muitos sabores
+ * @author allan
+ *
+ */
+
+@Embeddable
+@NoArgsConstructor @AllArgsConstructor
+public class SaborOrdemRecheioEmbeddedId implements Serializable{
+
+	@ManyToOne
+	@JoinColumn(name="ID_SABOR_FK", referencedColumnName="id_sabor")
+	@Getter @Setter private Sabor sabor;
+	
+	@ManyToOne
+	@JoinColumn(name="ID_RECHEIO_FK", referencedColumnName="id_recheio")
+	@Getter @Setter private Recheio recheio;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((recheio == null) ? 0 : recheio.hashCode());
+		result = prime * result + ((sabor == null) ? 0 : sabor.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SaborOrdemRecheioEmbeddedId other = (SaborOrdemRecheioEmbeddedId) obj;
+		if (recheio == null) {
+			if (other.recheio != null)
+				return false;
+		} else if (!recheio.equals(other.recheio))
+			return false;
+		if (sabor == null) {
+			if (other.sabor != null)
+				return false;
+		} else if (!sabor.equals(other.sabor))
+			return false;
+		return true;
+	}
+	
+	
+}
