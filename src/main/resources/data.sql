f7aeea9847b4dce3681bbfdb43098ce3fade9f6b
Allan Neri Fernandes da Silva <allan.nerifs@gmail.com>
2019-06-08 18:13:26 -0300

    Adicionado dados de inicialização

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,63 @@
+--- tamanhos ---
+
 INSERT INTO tb_tamanho (id_tamanho, nome, centimetros, numero_fatias, preco_padrao) VALUES ('d134a92e-889f-11e9-bc42-526af7764f64','Pequena', 25, 4, 17);
 INSERT INTO tb_tamanho (id_tamanho, nome, centimetros, numero_fatias, preco_padrao) VALUES ('d134aba4-889f-11e9-bc42-526af7764f64','Média', 30, 6, 22);
 INSERT INTO tb_tamanho (id_tamanho, nome, centimetros, numero_fatias, preco_padrao) VALUES ('d134ace4-889f-11e9-bc42-526af7764f64','Grande', 35, 8, 29);
 INSERT INTO tb_tamanho (id_tamanho, nome, centimetros, numero_fatias, preco_padrao) VALUES ('d134ae10-889f-11e9-bc42-526af7764f64','Gigante', 40, 12, 40);
+
+--- recheios ---
+
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5facf00-8a2e-11e9-bc42-526af7764f64', 'Abacaxi', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fad176-8a2e-11e9-bc42-526af7764f64', 'Alho Torrado', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fad2ca-8a2e-11e9-bc42-526af7764f64', 'Atum', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fad400-8a2e-11e9-bc42-526af7764f64', 'Azeitona', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fad7fc-8a2e-11e9-bc42-526af7764f64', 'Azeitonas Fatiadas', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fad95a-8a2e-11e9-bc42-526af7764f64', 'Bacalhau Desfiado', true, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fada86-8a2e-11e9-bc42-526af7764f64', 'Bacon', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fadba8-8a2e-11e9-bc42-526af7764f64', 'Banana', false, true, false);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fadcca-8a2e-11e9-bc42-526af7764f64', 'Batata Palha', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fade46-8a2e-11e9-bc42-526af7764f64', 'Beijinho', false, true, false);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fadf72-8a2e-11e9-bc42-526af7764f64', 'Brócolis', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fae332-8a2e-11e9-bc42-526af7764f64', 'Calabresa', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fae47c-8a2e-11e9-bc42-526af7764f64', 'Calabresa Triturada', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fae5b2-8a2e-11e9-bc42-526af7764f64', 'Camarão', true, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fae6d4-8a2e-11e9-bc42-526af7764f64', 'Canela', false, true, false);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fae7f6-8a2e-11e9-bc42-526af7764f64', 'Carne De Sol', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fae918-8a2e-11e9-bc42-526af7764f64', 'Carne Moída', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5faea3a-8a2e-11e9-bc42-526af7764f64', 'Cebola', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5faedbe-8a2e-11e9-bc42-526af7764f64', 'Chanpignon', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5faef12-8a2e-11e9-bc42-526af7764f64', 'Cheddar', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5faf03e-8a2e-11e9-bc42-526af7764f64', 'Chocolate Cremoso', false, true, false);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5faf160-8a2e-11e9-bc42-526af7764f64', 'Chocolate Em Pó', false, true, false);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5faf282-8a2e-11e9-bc42-526af7764f64', 'Chocolate Granulado', false, true, false);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5faf3a4-8a2e-11e9-bc42-526af7764f64', 'Côco Ralado', false, true, false);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5faf606-8a2e-11e9-bc42-526af7764f64', 'Cream Cheese', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5faf9c6-8a2e-11e9-bc42-526af7764f64', 'Creme De Camarão', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fafb10-8a2e-11e9-bc42-526af7764f64', 'Doce De Leite', false, true, false);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fafca0-8a2e-11e9-bc42-526af7764f64', 'Ervilha', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fafdd6-8a2e-11e9-bc42-526af7764f64', 'Figo', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5faff0c-8a2e-11e9-bc42-526af7764f64', 'Frango', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb0038-8a2e-11e9-bc42-526af7764f64', 'Goiabada Cremosa', false, true, false);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb0448-8a2e-11e9-bc42-526af7764f64', 'Gorgonzola', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb05a6-8a2e-11e9-bc42-526af7764f64', 'Leite Condensado', false, true, false);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb06c8-8a2e-11e9-bc42-526af7764f64', 'Lombo Fatiado', true, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb07f4-8a2e-11e9-bc42-526af7764f64', 'Manjericão Verde', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb0920-8a2e-11e9-bc42-526af7764f64', 'Milho', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb0a60-8a2e-11e9-bc42-526af7764f64', 'Molho', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb0b8c-8a2e-11e9-bc42-526af7764f64', 'Mussarela', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb0ee8-8a2e-11e9-bc42-526af7764f64', 'Orégano', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb1028-8a2e-11e9-bc42-526af7764f64', 'Ovos', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb115e-8a2e-11e9-bc42-526af7764f64', 'Palmito', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb1280-8a2e-11e9-bc42-526af7764f64', 'Parmesão', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb13a2-8a2e-11e9-bc42-526af7764f64', 'Peito De Peru', true, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb179e-8a2e-11e9-bc42-526af7764f64', 'Peperoni', true, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb18f2-8a2e-11e9-bc42-526af7764f64', 'Pêssego', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb1a14-8a2e-11e9-bc42-526af7764f64', 'Pimentão', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb1b36-8a2e-11e9-bc42-526af7764f64', 'Presunto', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb1c4e-8a2e-11e9-bc42-526af7764f64', 'Provolone', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb1e4c-8a2e-11e9-bc42-526af7764f64', 'Requeijão', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb1fb4-8a2e-11e9-bc42-526af7764f64', 'Rúcula', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb2342-8a2e-11e9-bc42-526af7764f64', 'Salsicha', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb248c-8a2e-11e9-bc42-526af7764f64', 'Tiras De Filé', true, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb25b8-8a2e-11e9-bc42-526af7764f64', 'Tomate', false, true, true);
+INSERT INTO tb_recheio (id_recheio, nome, eh_especial, eh_disponivel, eh_salgado) VALUES ('a5fb26da-8a2e-11e9-bc42-526af7764f64', 'Tomate Seco', false, true, true);
