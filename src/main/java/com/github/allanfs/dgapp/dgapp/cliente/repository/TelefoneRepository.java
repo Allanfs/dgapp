91f11eac258590ff4ad1cd9b2686bdc0e050a003
Allan Neri Fernandes da Silva <allan.fernandes@dataprev.gov.br>
2019-06-12 09:18:09 -0300

    #7 adicionado métodos no Repository
    encontrar pelo número do telefone
    encontrar telefones de um cliente informando seu id

--- src/main/java/com/github/allanfs/dgapp/dgapp/cliente/repository/TelefoneRepository.java
+++ src/main/java/com/github/allanfs/dgapp/dgapp/cliente/repository/TelefoneRepository.java
@@ -1,4 +1,5 @@
 package com.github.allanfs.dgapp.dgapp.cliente.repository;
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,7 @@ import com.github.allanfs.dgapp.dgapp.cliente.model.Telefone;
 
 public interface TelefoneRepository extends JpaRepository<Telefone, UUID>{
 
+	Telefone findByNumero( String numero );
+
+	Set<Telefone> findByCliente( UUID idCliente );
 }
