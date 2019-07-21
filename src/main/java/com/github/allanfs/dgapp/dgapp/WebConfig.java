bc1eaef8111b97b398074f61b64cdcdfe984429a
Allan Neri Fernandes da Silva <allan.nerifs@gmail.com>
2019-07-21 11:35:11 -0300

    Adicionado para acessar swagger

--- src/main/java/com/github/allanfs/dgapp/dgapp/WebConfig.java
+++ src/main/java/com/github/allanfs/dgapp/dgapp/WebConfig.java
@@ -3,14 +3,23 @@ package com.github.allanfs.dgapp.dgapp;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
- 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "POST","PUT", "DELETE").allowedOrigins("/**");
-    }
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("/**");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 }
